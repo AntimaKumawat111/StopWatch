4e922e68ae57145956ff9d9b8ce9ae61480895f4
Antimekumawat111 <aktank810@gmail.com>
2024-09-21 19:41:54 +0530

    fix js

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let hour = 0;
-  let mint = 0;
-  let second = 0;
-
-  document.getElementById("hour").innerHTML = hour;
-  document.getElementById("mint").innerHTML = mint;
-  document.getElementById("sec").innerHTML = second;
-  document.getElementsByClassName("showTime")[0].innerHTML =
-    hour + " : " + mint + " : " + second;
-});
+var hour = 0;
+var mint = 0 ;
+var sec = 0 ;
+var timer = false;
 
 function start() {
-  console.log("start");
+
+  timer = true;
+  stopwatch();
 }
 
 function stop() {
-  console.log("stop");
+  timer = false;
+}
+
+function stopwatch() {
+    if( timer == true)
+    {
+    sec = sec + 1;
+  }
+
+  if( sec == 60)
+  {
+    mint ++;;
+    sec = 0;
+  }
+
+  if( mint == 60)
+  {
+    hour ++;;
+    mint = 0;
+    sec = 0 ;
+  }
+
+  var hrString = hour;
+  var mintString = mint;
+  var secString = sec;
+
+  if( hour < 10)
+  {
+    hrString = "0" + hrString;
+  }
+
+  if( mint < 10)
+    {
+      mintString = "0" + mintString;
+    }
+
+  if( secString < 10)
+    {
+      secString = "0" + secString;
+    }
+
+  document.getElementById('hour').innerHTML = hrString;
+  document.getElementById('mint').innerHTML =" : "+ mintString;
+  document.getElementById('sec').innerHTML = " : " +secString;
+  setTimeout(stopwatch, 1000);
 }
