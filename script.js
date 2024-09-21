b58f0ed94847cf02fc962957459392f99f4d415c
Antimekumawat111 <aktank810@gmail.com>
2024-09-21 17:01:01 +0530

    edit css

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-  
-    function start() {
-        console.log("stop");
-    }
-    
-    function stop() {
-        console.log("stop");
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  let hour = 0;
+  let mint = 0;
+  let second = 0;
+
+  document.getElementById("hour").innerHTML = hour;
+  document.getElementById("mint").innerHTML = mint;
+  document.getElementById("sec").innerHTML = second;
+  document.getElementsByClassName("showTime")[0].innerHTML =
+    hour + " : " + mint + " : " + second;
+});
+
+function start() {
+  console.log("start");
+}
+
+function stop() {
+  console.log("stop");
+}
