321a3746eaebb69d1191152a1af7d07ea8761266
Antimekumawat111 <aktank810@gmail.com>
2024-09-22 10:30:58 +0530

    add restore function

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+
+
 var hour = 0;
-var mint = 0 ;
-var sec = 0 ;
+var mint = 0;
+var sec = 0;
 var timer = false;
 
 function start() {
-
   timer = true;
   stopwatch();
 }
@@ -13,46 +14,47 @@ function stop() {
   timer = false;
 }
 
+function restart() {
+  timer = false;
+  hour = 0;
+  mint = 0;
+  sec = 0;
+}
+
 function stopwatch() {
-    if( timer == true)
-    {
+  if (timer == true) {
     sec = sec + 1;
   }
 
-  if( sec == 60)
-  {
-    mint ++;;
+  if (sec == 60) {
+    mint++;
     sec = 0;
   }
 
-  if( mint == 60)
-  {
-    hour ++;;
+  if (mint == 60) {
+    hour++;
     mint = 0;
-    sec = 0 ;
+    sec = 0;
   }
 
-  var hrString = hour;
+  var hrString = hour; 
   var mintString = mint;
   var secString = sec;
 
-  if( hour < 10)
-  {
+  if (hour < 10) {
     hrString = "0" + hrString;
   }
 
-  if( mint < 10)
-    {
-      mintString = "0" + mintString;
-    }
+  if (mint < 10) {
+    mintString = "0" + mintString;
+  }
 
-  if( secString < 10)
-    {
-      secString = "0" + secString;
-    }
+  if (secString < 10) {
+    secString = "0" + secString;
+  }
 
-  document.getElementById('hour').innerHTML = hrString;
-  document.getElementById('mint').innerHTML =" : "+ mintString;
-  document.getElementById('sec').innerHTML = " : " +secString;
+  document.getElementById("hour").innerHTML = hrString;
+  document.getElementById("mint").innerHTML = " : " + mintString;
+  document.getElementById("sec").innerHTML = " : " + secString;
   setTimeout(stopwatch, 1000);
 }
